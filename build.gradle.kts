1a563ce03f4175c8ee4012c56c1b0b503a099799
Dmitry Seloustev <seloustevdv@elocont.ru>
2022-05-28 15:22:07 +0700

    Добавил потоки

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
+    idea
     id("org.springframework.boot") version "2.7.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+    id("com.google.protobuf") version "0.8.18"
 }
 
 group = "com.example"
@@ -13,6 +16,15 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+    google()
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("src/main/protobuf")
+        }
+    }
 }
 
 dependencies {
@@ -20,6 +32,16 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("net.devh:grpc-server-spring-boot-starter:2.13.1.RELEASE")
+
+    implementation("io.grpc:grpc-protobuf:1.46.0")
+    implementation("io.grpc:grpc-stub:1.46.0")
+    implementation("io.grpc:grpc-kotlin-stub:1.2.1")
+//    compileOnly("jakarta.annotation:jakarta.annotation-api:2.1.0") // Java 9+ compatibility - Do NOT update to 2.0.0
+    implementation("com.google.protobuf:protobuf-java:3.20.1")
+    implementation("com.google.protobuf:protobuf-kotlin:3.20.1")
+    implementation("net.devh:grpc-client-spring-boot-starter:2.13.1.RELEASE")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
 }
 
 tasks.withType<KotlinCompile> {
@@ -31,4 +53,29 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.20.1"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.46.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.2.1:jdk7@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
 }
--- src/main/kotlin/com/example/spring_grpc/services/RegistrationService.kt
+++ src/main/kotlin/com/example/spring_grpc/services/RegistrationService.kt
@@ -1,4 +1,34 @@
 package com.example.spring_grpc.services
 
-class RegistrationService {
+import Register
+import RegistrationServiceGrpcKt
+import com.google.protobuf.Int64Value
+import kotlinx.coroutines.flow.*
+import net.devh.boot.grpc.server.service.GrpcService
+import org.slf4j.LoggerFactory
+import registrationResult
+
+@GrpcService
+private class RegistrationService : RegistrationServiceGrpcKt.RegistrationServiceCoroutineImplBase() {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private val usersMap = mutableMapOf<Long, Register.User>()
+    private val usersFlow = MutableSharedFlow<Register.User>()
+
+    override suspend fun registerUser(request: Register.User): Register.RegistrationResult {
+        logger.debug(
+            "Registering userId:${request.id} user:${request.lastname} ${request.firstname} ${request.middlename}, " +
+                    "age: ${request.age}, gender: ${request.gender.name}"
+        )
+        usersMap[request.id] = request
+        usersFlow.emit(request)
+        return registrationResult { succeeded = true }
+    }
+
+    override suspend fun getUserById(request: Int64Value): Register.User {
+        return usersMap.getOrDefault(request.value, Register.User.getDefaultInstance())
+    }
+
+    override fun listUsers(request: Register.ListUsersInput): Flow<Register.User> {
+        return usersFlow
+    }
 }
